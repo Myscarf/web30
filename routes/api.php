bc254127e50b4ec93ee149a472c7ab7c21e1aab3
Myscarf <myscarftoo@gmail.com>
2021-06-06 10:56:53 +0300

    made CRUD API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,32 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//show the posts
+Route::get('/posts', function (){
+   return response()->json(\App\Post::all(), 200);
+});
+// show one post
+Route::get('/post/{id}', function ($id){
+    try{
+        $post = \App\Post::findOrFail($id);
+    }catch (Exception $exception){
+        return response()->json(['Msg' => $exception->getMessage()], 404);
+    }
+    return response()->json($post, 200);
+});
+
+Route::post('/post', 'APIAdminController@create');
+
+Route::put('/post/{id}', 'APIAdminController@update');
+
+Route::delete('/42b90196b487c54069097a68fe98ab6f/{id}', function($id){
+    try{
+        $post = \App\Post::findOrFail($id);
+    }catch (Exception $exception){
+        return response()->json(['Msg' => $exception->getMessage()], 404);
+    }
+    $post->delete();
+    return response()->json(['Msg' => $post->id . 'Пост удален'], 200);
+
+});
+
